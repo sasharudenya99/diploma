2bfa982d4c9a262dde6b3a09fe8af8d5f1e89f66
sasharudenya <sasha.rudenya@yandex.ru>
2019-09-01 09:33:56 +0300

    add administraion page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LectorsComponent } from './adminPanel/lectors/lectors.component';
+import { StudentsComponent } from './students/students.component';
+import { MainComponent } from './adminPanel/main/main.component';
+import { ResetThePasswordComponent } from './reset-the-password/reset-the-password.component';
+import { GroupComponent } from './adminPanel/group/group.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'main', component: MainComponent},
+  {path: 'lectors', component: LectorsComponent },
+  {path: 'students', component: StudentsComponent },
+  {path: 'resetPassword', component: ResetThePasswordComponent},
+  {path: 'groups', component: GroupComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/modal/lector-modal/lector-modal.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { LectorData } from '../../adminPanel/lectors/lectors.component';
+
+@Component({
+  selector: 'app-lector-modal',
+  templateUrl: './lector-modal.component.html',
+  styleUrls: ['./lector-modal.component.css']
+})
+export class LectorModalComponent implements OnInit {
+
+  ngOnInit(): void {
+  }
+
+  constructor(
+    public dialogRef: MatDialogRef<LectorModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: LectorData) { }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+
+}
